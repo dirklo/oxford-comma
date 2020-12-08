53e116079b4e4584bb8d39794ce02302ca9b86a8
Rick <dirklo@gmail.com>
2020-12-08 10:56:32 +0100

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
+    if array.length == 1
+        return array[0]
+    end
 
+    if array.length == 2
+        return "#{array[0]} and #{array[1]}"
+    end
+
+    string = ""
+    while array.length > 1
+        string.concat("#{array.shift()}, ")
+    end
+    string.concat("and #{array[0]}")
+    return string
 end
